65a214798d7295808a09fcc9ab4bcf59aec057f0
Jiaqi Wu <jwu724@wisc.edu>
2024-10-17 13:53:35 -0500

    fantasticjelly

---
+++ five_dirs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -r five
+mkdir five
+
+for i in {1..5}; do
+    mkdir five/dir$i
+    
+    for j in {1..4}; do
+        for k in $(seq $j); do
+            echo "$j" >> five/dir$i/file$j
+        done
+    done
+done
